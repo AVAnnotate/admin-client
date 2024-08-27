2c7ff91474160043935297ae216bab9c62515777
lorinjameson <lwjameson@gmail.com>
2024-08-27 10:40:43 -0400

    Fixing ts errors

--- test-vtt-export.ts
+++ test-vtt-export.ts
@@ -2,10 +2,14 @@
 import { annotationsToVtt, vttToAnnotations } from './src/lib/VTT/index.ts';
 // @ts-ignore
 import data from './data/test-annotations.json';
+import type { AnnotationEntry } from '@ty/Types.ts';
 
 const main = () => {
   //console.log(JSON.stringify(data));
-  const result = annotationsToVtt(data.annotations, 'speaker');
+  const result = annotationsToVtt(
+    data.annotations as AnnotationEntry[],
+    'speaker'
+  );
 
   console.log(result);
 
