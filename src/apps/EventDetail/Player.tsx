fa0657399dd7cf7319932a158f357ae41857e47c
Camden Mecklem <c@camdenmecklem.com>
2024-09-10 17:05:04 -0400

    WIP

--- src/apps/EventDetail/Player.tsx
+++ src/apps/EventDetail/Player.tsx
@@ -89,9 +89,8 @@ export const Player: React.FC<Props> = (props) => {
 
   return (
     <div className='player'>
-      {/* the player doesn't have any UI when playing audio files, so let's keep it 0x0 */}
-      {/* when we add video support, we'll need to conditionally set the width/height */}
       <ReactPlayer
+        controls={props.type === 'Video'}
         playing={playing}
         played={position / duration || 0}
         muted={muted}
@@ -105,71 +104,77 @@ export const Player: React.FC<Props> = (props) => {
         onReady={(player) => setPlayer(player)}
         progressInterval={50}
         url={props.url}
-        width={props.type === 'Video' ? 280 : 0}
-        height={props.type === 'Video' ? 200 : 0}
+        width={props.type === 'Video' ? '100%' : 0}
+        height={props.type === 'Video' ? '100%' : 0}
       />
-      <div className='player-control-panel'>
-        <div className='content'>
-          <Button
-            className='audio-button unstyled'
-            onClick={() => setPlaying(!playing)}
-          >
-            {playing ? <PauseFill color='black' /> : <PlayFill color='black' />}
-          </Button>
-          <div className='position-label'>
-            <span className='timestamp position'>{formattedPosition}</span>
-            <span>&nbsp;/&nbsp;</span>
-            <span className='timestamp duration'>{formattedDuration}</span>
-          </div>
-          <div className='seek-bar'>
-            <Slider.Root
-              className='seek-bar-slider'
-              defaultValue={[0]}
-              min={0}
-              max={0.999999999}
-              onValueChange={(val) => {
-                setSeeking(true);
-                setPosition(val[0] * duration);
-              }}
-              onValueCommit={onSeek}
-              step={0.0001}
-              value={[position / duration]}
+      {props.type === 'Audio' && (
+        <div className='player-control-panel'>
+          <div className='content'>
+            <Button
+              className='audio-button unstyled'
+              onClick={() => setPlaying(!playing)}
             >
-              <Slider.Track className='seek-bar-slider-track'>
-                <Slider.Range className='seek-bar-slider-range' />
-              </Slider.Track>
-              <Slider.Thumb className='seek-bar-slider-thumb' />
-            </Slider.Root>
+              {playing ? (
+                <PauseFill color='black' />
+              ) : (
+                <PlayFill color='black' />
+              )}
+            </Button>
+            <div className='position-label'>
+              <span className='timestamp position'>{formattedPosition}</span>
+              <span>&nbsp;/&nbsp;</span>
+              <span className='timestamp duration'>{formattedDuration}</span>
+            </div>
+            <div className='seek-bar'>
+              <Slider.Root
+                className='seek-bar-slider'
+                defaultValue={[0]}
+                min={0}
+                max={0.999999999}
+                onValueChange={(val) => {
+                  setSeeking(true);
+                  setPosition(val[0] * duration);
+                }}
+                onValueCommit={onSeek}
+                step={0.0001}
+                value={[position / duration]}
+              >
+                <Slider.Track className='seek-bar-slider-track'>
+                  <Slider.Range className='seek-bar-slider-range' />
+                </Slider.Track>
+                <Slider.Thumb className='seek-bar-slider-thumb' />
+              </Slider.Root>
+            </div>
+            <div>
+              <Tooltip.Provider>
+                <Tooltip.Root>
+                  <Tooltip.Trigger asChild>
+                    <Button
+                      className='unstyled copy-button'
+                      onClick={() => handleCopy(formattedPosition)}
+                    >
+                      <CopyIcon />
+                    </Button>
+                  </Tooltip.Trigger>
+                  <Tooltip.Content className='tooltip-content' side='bottom'>
+                    {t['Copy timestamp']}
+                  </Tooltip.Content>
+                </Tooltip.Root>
+              </Tooltip.Provider>
+            </div>
+            <Button
+              className='audio-button unstyled'
+              onClick={() => setMuted(!muted)}
+            >
+              {muted ? (
+                <VolumeMuteFill color='black' />
+              ) : (
+                <VolumeUpFill color='black' />
+              )}
+            </Button>
           </div>
-          <div>
-            <Tooltip.Provider>
-              <Tooltip.Root>
-                <Tooltip.Trigger asChild>
-                  <Button
-                    className='unstyled copy-button'
-                    onClick={() => handleCopy(formattedPosition)}
-                  >
-                    <CopyIcon />
-                  </Button>
-                </Tooltip.Trigger>
-                <Tooltip.Content className='tooltip-content' side='bottom'>
-                  {t['Copy timestamp']}
-                </Tooltip.Content>
-              </Tooltip.Root>
-            </Tooltip.Provider>
-          </div>
-          <Button
-            className='audio-button unstyled'
-            onClick={() => setMuted(!muted)}
-          >
-            {muted ? (
-              <VolumeMuteFill color='black' />
-            ) : (
-              <VolumeUpFill color='black' />
-            )}
-          </Button>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/apps/EventDetail/VideoDisplay.tsx
+++ src/apps/EventDetail/VideoDisplay.tsx
@@ -1,5 +1,89 @@
+import { useState } from 'react';
+import { AnnotationTable } from './AnnotationTable.tsx';
+import { AnnotationTableHeader } from './AnnotationTableHeader.tsx';
+import { EventHeader } from './EventHeader.tsx';
+import { Player } from './Player.tsx';
 import type { EventDisplayProps } from './types.ts';
+import { Button } from '@radix-ui/themes';
+import { PlusIcon } from '@radix-ui/react-icons';
+import { SetSelect } from './SetSelect.tsx';
 
 export const VideoDisplay: React.FC<EventDisplayProps> = (props) => {
-  return <p>hi</p>;
+  const { t } = props.i18n;
+
+  // position of the most recently clicked annotation
+  const [annoPosition, setAnnoPosition] = useState(0);
+
+  return (
+    <>
+      <div className='event-detail video-event-detail'>
+        <div className='video-pane'>
+          <EventHeader
+            avFileUuid={props.avFileUuid}
+            event={props.event}
+            eventUuid={props.eventUuid}
+            i18n={props.i18n}
+            projectSlug={props.projectSlug}
+            setAvFile={props.stateHandlers.setAvFile}
+            setShowEventDeleteModal={
+              props.stateHandlers.setShowEventDeleteModal
+            }
+          />
+          <Player
+            type='Video'
+            i18n={props.i18n}
+            url={props.event.audiovisual_files[props.avFileUuid].file_url}
+            position={annoPosition}
+          />
+        </div>
+        <div>
+          <div>
+            <h3>{t['Annotations']}</h3>
+            <div>
+              {props.sets.length > 1 && (
+                <SetSelect
+                  onChange={(uuid) =>
+                    props.stateHandlers.setCurrentSetUuid(uuid)
+                  }
+                  sets={props.sets}
+                  value={
+                    props.sets.find((set) => set.uuid === props.setUuid) ||
+                    props.sets[0]
+                  }
+                />
+              )}
+              <Button
+                className='primary add-set-button'
+                onClick={() => props.stateHandlers.setShowAddSetModal(true)}
+              >
+                {t['Add Set']}
+                <PlusIcon />
+              </Button>
+            </div>
+          </div>
+          <AnnotationTableHeader
+            displayAnnotations={props.displayAnnotations}
+            i18n={props.i18n}
+            onExport={props.onExport}
+            sets={props.sets}
+            setSearch={props.stateHandlers.setSearch}
+            setShowAnnoCreateModal={props.stateHandlers.setShowAnnoCreateModal}
+            setUuid={props.setUuid}
+            eventUuid={props.eventUuid}
+            projectSlug={props.projectSlug}
+          />
+          <div className='table-container'>
+            <AnnotationTable
+              i18n={props.i18n}
+              displayAnnotations={props.displayAnnotations}
+              project={props.project}
+              setDeleteAnnoUuid={props.stateHandlers.setDeleteAnnoUuid}
+              setEditAnnoUuid={props.stateHandlers.setEditAnnoUuid}
+              setAnnoPosition={setAnnoPosition}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
