1a320f6719b52b50fc10ffc9daf86731dbf01af7
lorinjameson <lwjameson@gmail.com>
2024-10-17 14:58:28 -0400

    Fix for Projects timeout

--- src/apps/Projects/Projects.tsx
+++ src/apps/Projects/Projects.tsx
@@ -1,4 +1,9 @@
-import type { AllProjects, Translations } from '@ty/Types.ts';
+import type {
+  AllProjects,
+  ProjectData,
+  Translations,
+  UserInfo,
+} from '@ty/Types.ts';
 import { Plus } from '@phosphor-icons/react/Plus';
 import { Header } from './Header/Header.tsx';
 import './Projects.css';
@@ -7,11 +12,15 @@ import { useEffect, useState } from 'react';
 import { ProjectFilter } from './Header/Header.tsx';
 import { ProjectsGrid } from './ProjectsGrid/ProjectsGrid.tsx';
 import { Sorters } from '@components/SortAction/index.ts';
+import { LoadingOverlay } from '@components/LoadingOverlay/LoadingOverlay.tsx';
 
+export type Repositories = { org: string; repo: string };
 export interface ProjectsProps {
-  repos: { org: string; repo: string }[];
+  repos: Repositories[];
 
   i18n: Translations;
+
+  userInfo: UserInfo;
 }
 
 export const Projects = (props: ProjectsProps) => {
@@ -21,6 +30,7 @@ export const Projects = (props: ProjectsProps) => {
   const [search, setSearch] = useState<string | undefined>();
   const [sort, setSort] = useState<'Name' | 'Oldest' | 'Newest'>('Name');
   const [projects, setProjects] = useState<AllProjects | undefined>();
+  const [saving, setSaving] = useState(false);
 
   const handleChangeFilter = (filter: ProjectFilter) => {
     setFilter(filter);
@@ -34,17 +44,48 @@ export const Projects = (props: ProjectsProps) => {
     setSort(sortFn);
   };
 
-  useEffect(() => {
-    if (props.repos) {
-      const allProjects: AllProjects = [];
+  const getAllProjects = async (repos: Repositories[]) => {
+    const allProjects: ProjectData[] = [];
+    for (let i = 0; i < repos.length; i++) {
+      const repo = repos[i];
+      const resp = await fetch(`/api/git-repos/${repo.org}/${repo.repo}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-      for (let i = 0; i < props.repos.length; i++) {
-        await;
+      if (resp.ok) {
+        const data = await resp.json();
+
+        allProjects.push(data);
       }
     }
-    const projs: AllProjects = JSON.parse(JSON.stringify(props.projects));
 
-    if (props.projects) {
+    return allProjects;
+  };
+
+  useEffect(() => {
+    if (props.repos) {
+      setSaving(true);
+      getAllProjects(props.repos).then((data) => {
+        const all: AllProjects = {
+          myProjects: data.filter(
+            (p) => p.project.creator === props.userInfo.profile.gitHubName
+          ),
+          sharedProjects: data.filter(
+            (p) => p.project.creator !== props.userInfo.profile.gitHubName
+          ),
+        };
+        setProjects(all);
+        setSaving(false);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (projects) {
+      const projs: AllProjects = JSON.parse(JSON.stringify(projects));
       if (sort === 'Name') {
         projs.myProjects.sort(Sorters['Name']);
         projs.sharedProjects.sort(Sorters['Name']);
@@ -64,6 +105,7 @@ export const Projects = (props: ProjectsProps) => {
 
   return (
     <div className='projects-container'>
+      {saving && <LoadingOverlay text={t['Loading Your Projects']} />}
       <div className='projects-header-bar'>
         <h1>{t['Projects']}</h1>
         <Button
--- src/components/LoadingOverlay/LoadingOverlay.tsx
+++ src/components/LoadingOverlay/LoadingOverlay.tsx
@@ -5,10 +5,15 @@ import './LoadingOverlay.css';
 /**
  * A full-screen loading indicator that makes the content underneath inert.
  */
-export const LoadingOverlay = () => {
+
+interface LoadingOverlayProps {
+  text?: string;
+}
+export const LoadingOverlay = (props: LoadingOverlayProps) => {
   return (
     <div className='loading-overlay'>
       <Spinner />
+      <h1>{props.text}</h1>
     </div>
   );
 };
