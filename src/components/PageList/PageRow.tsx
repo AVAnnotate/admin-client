0d060583293a6c8d516e74c203afe8cb2bce484b
lorinjameson <lwjameson@gmail.com>
2024-10-23 15:58:34 -0400

    Open page on Pages tab

--- src/components/PageList/PageRow.tsx
+++ src/components/PageList/PageRow.tsx
@@ -42,13 +42,25 @@ export const PageRow: React.FC<Props> = (props) => {
     {
       label: t['Open'],
       icon: BoxArrowUpRight,
-      onClick: () =>
-        window.open(
-          `https://${props.project.project.github_org}.github.io/${
+      onClick: () => {
+        // See what kind of page this is
+        const type = props.project.pages[props.uuid].autogenerate.enabled
+          ? props.project.pages[props.uuid].autogenerate.type
+          : 'page';
+        let url;
+        if (type === 'home') {
+          url = `https://${props.project.project.github_org}.github.io/${props.project.project.slug}/`;
+        } else if (type === 'event') {
+          url = `https://${props.project.project.github_org}.github.io/${
             props.project.project.slug
-          }/${props.project.pages[props.uuid].slug || props.uuid}`,
-          '_blank'
-        ),
+          }/events/${props.project.pages[props.uuid].slug || props.uuid}`;
+        } else {
+          url = `https://${props.project.project.github_org}.github.io/${
+            props.project.project.slug
+          }/pages/${props.project.pages[props.uuid].slug || props.uuid}`;
+        }
+        return window.open(url, '_blank');
+      },
     },
   ];
 
@@ -57,13 +69,25 @@ export const PageRow: React.FC<Props> = (props) => {
       {
         label: t['Open'],
         icon: BoxArrowUpRight,
-        onClick: () =>
-          window.open(
-            `https://${props.project.project.github_org}.github.io/${
+        onClick: () => {
+          // See what kind of page this is
+          const type = props.project.pages[props.uuid].autogenerate.enabled
+            ? props.project.pages[props.uuid].autogenerate.type
+            : 'page';
+          let url;
+          if (type === 'home') {
+            url = `https://${props.project.project.github_org}.github.io/${props.project.project.slug}/`;
+          } else if (type === 'event') {
+            url = `https://${props.project.project.github_org}.github.io/${
               props.project.project.slug
-            }/${props.project.pages[props.uuid].slug || props.uuid}`,
-            '_blank'
-          ),
+            }/events/${props.project.pages[props.uuid].slug || props.uuid}`;
+          } else {
+            url = `https://${props.project.project.github_org}.github.io/${
+              props.project.project.slug
+            }/pages/${props.project.pages[props.uuid].slug || props.uuid}`;
+          }
+          return window.open(url, '_blank');
+        },
       },
       {
         label: t['Edit'],
@@ -84,6 +108,7 @@ export const PageRow: React.FC<Props> = (props) => {
       options.push({
         label: t['Re-Enable Auto-Generation'],
         icon: FiletypeHtml,
+        // @ts-ignore
         onClick: () => props.onReEnableAutoGeneration(),
       });
     }
@@ -103,6 +128,7 @@ export const PageRow: React.FC<Props> = (props) => {
       options.push({
         label: t['Delete'],
         icon: Trash,
+        // @ts-ignore
         onClick: async () => props.onDelete(),
       });
     }
