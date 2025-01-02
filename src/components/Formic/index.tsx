571b4d2df1bb6666860ef2ffc12a3afa7064ca13
Rebecca Black <rebecca.ann.black@gmail.com>
2025-01-02 13:21:36 -0500

    remove commented-out code

--- src/components/Formic/index.tsx
+++ src/components/Formic/index.tsx
@@ -131,7 +131,6 @@ export const TimeInput = (props: TimeInputProps) => {
       return `${hours.toString().padStart(2, '0')}:${minutes
         .toString()
         .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-      // return new Date((seconds || 0) * 1000).toISOString().slice(11, 19);
     },
     []
   );
