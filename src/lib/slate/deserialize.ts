afc2114ef90deec28b899796ae7c0f59e9aaeae3
lorinjameson <lwjameson@gmail.com>
2025-04-14 14:21:39 -0400

    Paste HTML

--- src/lib/slate/deserialize.ts
+++ src/lib/slate/deserialize.ts
@@ -74,3 +74,85 @@ export const deserialize = (
       return children;
   }
 };
+
+interface ElementAttributes {
+  type: any;
+  url?: string;
+}
+
+const ELEMENT_TAGS: Record<string, (el: HTMLElement) => ElementAttributes> = {
+  A: (el) => ({ type: 'link', url: el.getAttribute('href')! }),
+  BLOCKQUOTE: () => ({ type: 'block-quote' }),
+  H1: () => ({ type: 'heading-one' }),
+  H2: () => ({ type: 'heading-two' }),
+  H3: () => ({ type: 'heading-three' }),
+  H4: () => ({ type: 'heading-four' }),
+  H5: () => ({ type: 'heading-four' }),
+  H6: () => ({ type: 'heading-four' }),
+  IMG: (el) => ({ type: 'image', url: el.getAttribute('src')! }),
+  LI: () => ({ type: 'list-item' }),
+  OL: () => ({ type: 'numbered-list' }),
+  P: () => ({ type: 'paragraph' }),
+  PRE: () => ({ type: 'code-block' }),
+  UL: () => ({ type: 'bulleted-list' }),
+};
+
+interface TextAttributes {
+  code?: boolean;
+  strikethrough?: boolean;
+  italic?: boolean;
+  bold?: boolean;
+  underline?: boolean;
+}
+
+const TEXT_TAGS: Record<string, () => TextAttributes> = {
+  CODE: () => ({ code: true }),
+  DEL: () => ({ strikethrough: true }),
+  EM: () => ({ italic: true }),
+  I: () => ({ italic: true }),
+  S: () => ({ strikethrough: true }),
+  STRONG: () => ({ bold: true }),
+  U: () => ({ underline: true }),
+};
+
+export const deserializeHtml = (el: HTMLElement | ChildNode): any => {
+  if (el.nodeType === 3) {
+    return el.textContent;
+  } else if (el.nodeType !== 1) {
+    return null;
+  } else if (el.nodeName === 'BR') {
+    return '\n';
+  }
+
+  const { nodeName } = el;
+  let parent = el;
+
+  if (
+    nodeName === 'PRE' &&
+    el.childNodes[0] &&
+    el.childNodes[0].nodeName === 'CODE'
+  ) {
+    parent = el.childNodes[0];
+  }
+  let children = Array.from(parent.childNodes).map(deserializeHtml).flat();
+
+  if (children.length === 0) {
+    children = [{ text: '' }];
+  }
+
+  if (el.nodeName === 'BODY') {
+    return jsx('fragment', {}, children);
+  }
+
+  if (ELEMENT_TAGS[nodeName]) {
+    const attrs = ELEMENT_TAGS[nodeName](el as HTMLElement);
+    return jsx('element', attrs, children);
+  }
+
+  if (TEXT_TAGS[nodeName]) {
+    const attrs = TEXT_TAGS[nodeName]();
+    return children.map((child) => jsx('text', attrs, child));
+  }
+
+  return children;
+};
