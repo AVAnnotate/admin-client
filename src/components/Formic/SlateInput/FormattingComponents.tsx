6c4e8e592619c42738f84212c0ce081eea4ce07d
Camden Mecklem <c@camdenmecklem.com>
2025-06-27 11:51:32 -0400

    fix perpetual linkage

--- src/components/Formic/SlateInput/FormattingComponents.tsx
+++ src/components/Formic/SlateInput/FormattingComponents.tsx
@@ -7,6 +7,7 @@ import * as Dialog from '@radix-ui/react-dialog';
 import type { AVAEditor, ImageData, ImageSize } from '@ty/slate.ts';
 import type { Translations } from '@ty/Types.ts';
 import { ToolbarTooltip } from './ToolbarTooltip.tsx';
+import { Editor } from 'slate';
 
 export const HighlightColorButton = (props: SlateButtonProps) => {
   const editor = useSlate();
@@ -87,6 +88,8 @@ export const LinkButton = (props: LinkDialogProps) => {
     setOpen(false);
   };
 
+  const isActive = Editor.marks(editor)?.link;
+
   return (
     <Dialog.Root open={open}>
       <Dialog.Trigger asChild>
@@ -96,10 +99,16 @@ export const LinkButton = (props: LinkDialogProps) => {
         >
           <Button
             className={`link-button unstyled ${
-              highlightedText ? '' : 'disabled-link-button'
+              isActive ? 'activated-link-button' : ''
             }`}
-            disabled={!highlightedText}
-            onClick={() => setOpen(true)}
+            onMouseDown={(event) => {
+              event.preventDefault();
+              if (isActive) {
+                Editor.removeMark(editor, 'link');
+              } else {
+                setOpen(true);
+              }
+            }}
             type='button'
           >
             <props.icon />
